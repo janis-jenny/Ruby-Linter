aa82cdcca36692258b0951b853cc8e5779a6454d
janis-jenny <pcardenasvm@gmail.com>
2020-10-30 10:30:25 -0500

    edit check trailing space method

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
 #init interaction between the user and the program
+require_relative 'file_reader.rb'
 
-FileReader.new (./style.css)   #ARGV.first is to write the file on the terminal
+FileReader.new ('./style.css')   #ARGV.first is to write the file on the terminal
--- lib/checks.rb
+++ lib/checks.rb
@@ -10,8 +10,15 @@ class CheckErrror
 
     def check_trailing_space
         @checker.each_with_index do |element, line|
-                if element[-2] == '' && !element.empty?
-                    @errors << "line: #{element +1}: #{line.size -1}: Error: Trailing whitespace detected"
-                end
+          if element[-1] == ' ' && !element.empty?
+              @errors << "line: #{line +1}}: Error: Trailing whitespace detected"
+          end
     end
 end
+
+#spaces around the comment 2 checks for the beggining for the comment and at the end
+#space befor open a bracket 
+#check if after a colon there is a space
+#method to chech a space at the end of semicolon and bracket
+
+use the index method to get the specific character
