aa9001d51d0f8d4990c18680fd74007f3ec84eb7
janis-jenny <pcardenasvm@gmail.com>
2020-10-30 14:15:22 -0500

    add check space before and after comment method and edit file reader and main

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,16 @@
 #init interaction between the user and the program
-require_relative 'file_reader.rb'
+require_relative '../lib/checks.rb'
 
-FileReader.new ('./style.css')   #ARGV.first is to write the file on the terminal
+#file_object = FileReader.new ('./style.css')   #ARGV.first is to write the file on the terminal
+#p file_object.file.readlines(chomp: true)[0].split #chomp: true deletes the new line thing \n
+check_error = CheckErrror.new('./style.css') #is an objet
+check_error.check_trailing_space
+check_error.check_space_after_colon
+check_error.check_space_before_open_bracket
+check_error.check_before_comment
+check_error.check_after_comment
+puts check_error.errors
+
+#create a loop to find all the css files and then run all the methods on each file
+#then u can puts the errors array
+
--- lib/checks.rb
+++ lib/checks.rb
@@ -1,7 +1,7 @@
 require_relative 'file_reader.rb'
 
 class CheckErrror
-  attr_reader: @checker
+  attr_reader :checker, :errors
 
   def initialize(file_path)
     @checker = FileReader.new(file_path)
@@ -9,36 +9,61 @@ class CheckErrror
   end
   #if you want to remove only leading and trailing whitespace (like PHP's trim) you can use .strip, but if you want to remove all whitespace, you can use .gsub(/\s+/, "") instead 
   def check_trailing_space #this means only at the end
-    @checker.each_with_index do |element, line|
+    @checker.file.each_with_index do |element, line|
       if element[-1] == ' ' && !element.empty?
-          @errors << "line: #{line +1}}: Error: Trailing whitespace detected" + #" '#{line.gsub(/\s+/, "")}' "
+          @errors << "line: #{line +1}: Error: Trailing whitespace detected" 
       end 
+    end
   end
-  
+
   def check_space_after_colon 
-    @checker.each_with_index do |element, line|
+    @checker.file.each_with_index do |element, line|
       if element.include?(':') 
         index_colon = element.index(':')
         unless element[index_colon + 1] == ' '
-        @errors << "line: #{line + 1}}: Error: Missing space after colon"
+        @errors << "line: #{line + 1}: Error: Missing space after colon"
         end
       end 
+    end
   end
 
   def check_space_before_open_bracket
-    @checker.each_with_index do |element, line|
+    @checker.file.each_with_index do |element, line|
       if element.include?('{') 
         index_bracket = element.index('{')
         unless element[index_bracket - 1] == ' ' || index_bracket == 0
-          @errors << "line: #{line + 1}}: Error: Missing space before the bracket"
+          @errors << "line: #{line + 1}: Error: Missing space before the bracket"
         end
       end 
+    end
   end
+
+  def check_before_comment
+    @checker.file.each_with_index do |element, line|
+      if element.include?('/*') 
+        index_char = element.index('*')
+        unless element[index_char + 1] == ' ' 
+          @errors << "line: #{line + 1}: Error: Missing space before the comment"
+        end
+      end 
+    end
+  end
+
+  def check_after_comment
+    @checker.file.each_with_index do |element, line|
+      if element.include?('*/') 
+        element.slice!('/*') #the new string or result of the method, is gonna stored inside the element again
+        index_comment = element.index('*')
+        unless element[index_comment - 1] == ' ' || index_comment == 0
+          @errors << "line: #{line + 1}: Error: Missing space after the comment"
+        end
+      end 
+    end
+  end
+  #spaces around the comment 2 checks for the beggining for the comment and at the end
+  #space befor open a bracket 
+  #check if after a colon there is a space
+  #method to chech a space at the end of semicolon and bracket
+
+  # use the index method to get the specific character
 end
-
-#spaces around the comment 2 checks for the beggining for the comment and at the end
-#space befor open a bracket 
-#check if after a colon there is a space
-#method to chech a space at the end of semicolon and bracket
-
-use the index method to get the specific character
