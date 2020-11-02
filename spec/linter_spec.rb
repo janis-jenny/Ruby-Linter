62e16649b40c45f535b92e22d0167e8ede38baec
janis-jenny <pcardenasvm@gmail.com>
2020-11-02 06:46:30 -0500

    add testing rspec third method

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -15,5 +15,19 @@ describe CheckErrror do
       expect(checks.check_space_after_colon ).to eq(true)
     end
   end
+
+  describe '#check_space_after_colon ' do
+    it 'should return a error message when there is not an empty space after colon' do
+      expect(checks.check_space_after_colon ).to eq(true)
+    end
+  end
+
+  describe '#check_space_before_open_bracket' do
+    it 'should return a error message when there is not an empty space before open bracket' do
+      expect(checks.check_space_before_open_bracket).to eq(true)
+    end
+  end
+
+
   
 end
