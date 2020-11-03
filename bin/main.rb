930c2beaae7897a7393961042ff1fed2cbff2309
janis-jenny <pcardenasvm@gmail.com>
2020-11-03 13:47:13 -0500

    add a StandardError message on main

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,7 @@ require 'colorize'
 require_relative '../lib/checks'
 
 ARGV.each do |path|
-  next unless File.exist?(path) && File.extname(path) == '.css'
+  raise StandardError, 'File does NOT exist' unless File.exist?(path) && File.extname(path) == '.css'
 
   check_error = CheckErrror.new(path)
   check_error.check_trailing_space
@@ -11,4 +11,5 @@ ARGV.each do |path|
   check_error.check_before_comment
   check_error.check_after_comment
   puts check_error.errors
+
 end
--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -2,13 +2,23 @@ require_relative '../lib/checks'
 
 describe CheckErrror do
   let(:checks) { CheckErrror.new('style.css') }
-  let(:line) { '1' }
 
   describe '#check_trailing_space' do
-    it 'should return a error message when there is an empty space at the end of the line' do
+    line = 6
+    it 'should return a error message when there is an empty space at the end of the line 6' do
       expect(checks.check_trailing_space).to eq(true)
     end
   end
+
+  describe '#check_trailing_space' do
+    element = 0
+    if element[-1] != ' ' 
+      it 'should return nill when there is no an empty space at the end of the line 6' do
+        expect(checks.check_trailing_space).to be(nil)
+      end
+    end
+  end
+
 
   describe '#check_space_after_colon ' do
     it 'should return a error message when there is not an empty space after colon' do
